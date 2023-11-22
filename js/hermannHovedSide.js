4d02e40caa057b31ec7d5234657330557cd7d56f
Hermann Karl <142892325+HermannKarl@users.noreply.github.com>
2023-11-22 14:35:14 +0100

    Update hermannHovedSide.js

--- js/hermannHovedSide.js
+++ js/hermannHovedSide.js
@@ -1,12 +1,12 @@
-rectangle(ctx, 450, 40, 75, 75, 'solid', 'chartreuse');
-        	rectangle(ctx, 485, 115, 75, 75, 'solid', 'blue');
-		rectangle(ctx, 410, 115, 75, 75, 'solid', 'red');
+rectangle(ctx, 950, 40, 75, 75, 'solid', 'chartreuse');
+        	rectangle(ctx, 985, 115, 75, 75, 'solid', 'blue');
+		rectangle(ctx, 910, 115, 75, 75, 'solid', 'red');
 			
 			
             	// Draw using SVG functions
-            	const rSVG = rectangleSVG(450, 40, 75, 75, 'solid', 'chartreuse');
-            	const r2SVG = rectangleSVG(485, 115, 75, 75, 'solid', 'blue');
-            	const r3SVG = rectangleSVG(410, 115, 75, 75, 'solid', 'red');
+            	const rSVG = rectangleSVG(950, 40, 75, 75, 'solid', 'chartreuse');
+            	const r2SVG = rectangleSVG(985, 115, 75, 75, 'solid', 'blue');
+            	const r3SVG = rectangleSVG(910, 115, 75, 75, 'solid', 'red');
         
 		
 		// Append the SVG elements to the SVG canvas
