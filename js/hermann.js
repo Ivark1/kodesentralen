4c4dbd46748405dd9ee4c5469747446fe8d132ce
Hermann Karl <142892325+HermannKarl@users.noreply.github.com>
2023-11-22 14:13:31 +0100

    Update hermann.js

--- js/hermann.js
+++ js/hermann.js
@@ -1,1 +1,17 @@
-
+const ctx = initCanvas('brett'); // Initialiserer canvaset
+		rectangle(ctx, 150, 40, 75, 75, 'solid', 'chartreuse');
+        	rectangle(ctx, 185, 115, 75, 75, 'solid', 'blue');
+		rectangle(ctx, 110, 115, 75, 75, 'solid', 'red');
+			
+            	const svgCanvas = document.getElementById('svgBrett');
+			
+            	// Draw using SVG functions
+            	const rSVG = rectangleSVG(150, 40, 75, 75, 'solid', 'chartreuse');
+            	const r2SVG = rectangleSVG(185, 115, 75, 75, 'solid', 'blue');
+            	const r3SVG = rectangleSVG(110, 115, 75, 75, 'solid', 'red');
+        
+		
+		// Append the SVG elements to the SVG canvas
+            	svgCanvas.appendChild(rSVG);
+            	svgCanvas.appendChild(r2SVG);
+		svgCanvas.appendChild(r3SVG);
