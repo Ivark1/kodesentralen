a1474a16e7161423e04c2dca2e5df2c45bff9a16
Ivar Kaldheim <ivark@uia.no>
2023-11-22 14:24:43 +0100

    oppdatert

--- js/ivarHovedside.js
+++ js/ivarHovedside.js
@@ -1,13 +1,13 @@
 
-rectangle(ctx, 700, 400, 350, 200,  'solid', 'black'); 
-rectangle(ctx, 750, 400, 240, 200, 'solid', 'blue'); 
-rectangle(ctx, 750, 450, 240, 100, 'solid', 'red'); 
-circle(ctx, 820, 450, 100, 'solid', 'chartreuse');
+rectangle(ctx, 700, 500, 350, 200,  'solid', 'black'); 
+rectangle(ctx, 750, 500, 240, 200, 'solid', 'blue'); 
+rectangle(ctx, 750, 550, 240, 100, 'solid', 'red'); 
+circle(ctx, 820, 550, 100, 'solid', 'chartreuse');
 
 // Tegner med SVG
 const rect1IKSVG = rectangleSVG(700, 200, 350, 200, 'solid','black');
 const rect2IKSVG = rectangleSVG(750, 200, 240, 200, 'solid', 'blue');
-const rect3IKSVG = rectangleSVG(750, 250, 240, 100, 'solid', 'red');
+const rect3IKSVG = rectangleSVG(750, 350, 240, 100, 'solid', 'red');
 const circle1IKSVG = circleSVG(875, 300, 50,  'solid', 'chartreuse');
             
 
